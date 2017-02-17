7145bdd950c7cb62e663e45de9e66a90f2813064
chenqiaoen521 <274078815@qq.com>
2017-02-17 19:08:50 +0800

    day9
    day9

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -34,7 +34,33 @@
 					</div>
 					<div class="content">
 						<h1 class="title infos-title">商品评价</h1>
-						<v-ratingselect></v-ratingselect>
+						<v-ratingselect @ratingSselect="changeType"  @ratingsOnlycont=changeCont  :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></v-ratingselect>
+						<div class="rating-wrapper">
+							<ul v-show="food.ratings && food.ratings.length">
+								<li v-for="rating in food.ratings" class="rating-item border-1px">
+									<div class="user">
+										<span class="name">{{rating.username}}}</span>
+										<img :src="rating.avatar" width=12 height=12 :alt="rating.username" class="avatar">
+									</div>
+									<div class="time">{{rating.rateTime}}</div>
+									<p class="text">
+										<span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+									</p>
+								</li>
+							</ul>
+							<div class="no-rating"  v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+						</div>
+					</div>
+					
+					<div class="content">
+						<h2 class="title infos-title">商品介绍</h2>
+						<div class="infos-detail">{{food.info}}</div>
+						<v-split></v-split>
+					</div>
+					<div class="content">
+						<h2 class="title infos-title">商品介绍</h2>
+						<div class="infos-detail">{{food.info}}</div>
+						<v-split></v-split>
 					</div>
 				</div>
 			</div>
@@ -74,6 +100,8 @@ export default {
 	methods: {
 		show () {
 			this.showFlag = !this.showFlag
+			this.selectType = ALL
+			this.onlyContent = true
 			this.$nextTick(() => {
 			if (!this.detailScroll) {
 					this.detailScroll = new BScroll(this.$el, {
@@ -91,6 +119,12 @@ export default {
 		},
 		addDone (e) {
 			this.$emit('add', e)
+		},
+		changeType (type) {
+			this.selectType = type
+		},
+		changeCont (flag) {
+			this.onlyContent = flag
 		}
 	}
 }
@@ -121,6 +155,7 @@ export default {
 			left:0
 			width:100%
 			height:100%
+			filter:blur(1px)
 	.wrap-back
 		position:absolute
 		top:10px
@@ -194,5 +229,41 @@ export default {
 			font-size:12px
 			line-height:24px
 			color:rgb(77,85,93)
-		
-</style>
+		.rating-wrapper
+			padding:0 18px
+			.rating-item
+				position:relative
+				padding: 16px 0
+				border-1px(rgba(7,17,27,.1))
+				.user
+					position:absolute
+					right:0
+					top:16px
+					font-size:0
+					line-height:12px
+					.name
+						display:inline-block
+						margin-right:6px
+						vertical-align:top
+						font-size:10px
+						color:rgb(147,153,159)
+					.avatar
+						border-radius:50%
+				.time
+					mragin-bottom:6px
+					line-height:12px
+					font-size:10px
+					color:rgb(147,153,159)
+				.text
+					line-height:16px
+					font-size:12px
+					color:rgb(7,17,27)
+					.icon-thumb_up,.icon-thumb_down
+						margin-right:4px
+						line-height:16px
+						font-size:12px
+					.icon-thumb_up
+						color:rgb(0,160,220)
+					.icon-thumb_down
+						color:rgb(147,153,159)
+</style>			
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="ratingselect">
-		<div class="rating-type">
-			<span>{{desc.all}}</span>
-			<span>{{desc.positive}}</span>
-			<span>{{desc.negative}}</span>
+		<div class="rating-type border-1px">
+			<span class="block positive" :class="{'active':selectType==2}" @click="select(2, $event)">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+			<span class="block positive" :class="{'active':selectType==0}" @click="select(0, $event)">{{desc.positive}}<span class="count">{{positives.length}}</span></span>
+			<span class="block negative" :class="{'active':selectType==1}" @click="select(1, $event)">{{desc.negative}}<span class="count">{{negative.length}}</span></span>
 		</div>
-		<div class="switch">
+		<div class="switch"  :class="{'on':onlyContent}"  @click="toggleContent">
 			<span class="icon-check_circle"></span><span class="text">只看有内容的评价</span>
 		</div>
 	</div>
@@ -15,11 +15,6 @@ const POSITIVE = 0
 const NEGATIVE = 1
 const ALL = 2
 export default {
-	data() {
-		return {
-
-		}
-	},
 	props: {
 		ratings: {
 			type: Array,
@@ -45,18 +40,89 @@ export default {
 				}
 			}
 		}
+	},
+	methods: {
+		select (type, event) {
+			if (!event._constructed) {
+				return
+			}
+			this.$emit('ratingSselect', type)
+		},
+		toggleContent (e) {
+			if (!e._constructed) {
+				return
+			}
+			this.$emit('ratingsOnlycont', !this.onlyContent)
+		}
+	},
+	computed: {
+		positives () {
+			let positi = []
+			this.ratings.forEach((rating) => {
+				if (rating.rateType === POSITIVE) {
+					positi.push(rating.rateType)
+				}
+			})
+			return positi
+		},
+		negative () {
+			let negat = []
+			this.ratings.forEach((rating) => {
+				if (rating.rateType === NEGATIVE) {
+					negat.push(rating.rateType)
+				}
+			})
+			return negat
+		}
 	}
 }
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
+@import "../../common/stylus/mixin.styl"
 .ratingselect
-	padding:6px 18px 118px
-	.ratings-icon
-		border-radius:12px
-		line-height:16px
-		font-size:12px
-		margin-right:8px
-	.all
-		color:rgb(255,255,255)
-		background-color:rgb(0,160,220,.2)
+	.rating-type
+		font-size:0px
+		padding:12px 0
+		margin:0 18px
+		border-1px(rgba(7,17,27,.1))
+		.block
+			display:inline-block
+			padding:8px 12px
+			font-size:12px
+			margin-right:8px
+			border-radius:1px
+			color:rgb(77,85,93)
+			line-height:16px
+			.count
+				font-size:8px
+				margin-left:2px
+			&.positive
+				color:rgb(77,85,93)
+				background-color:rgba(0,160,220,.2)
+				&.active
+					color:#fff
+					background-color:rgb(0,160,220)
+			&.negative
+				color:rgb(77,85,93)
+				background-color:rgba(77,85,93,.2)
+				&.active
+					color:#fff
+					background-color:rgb(77,85,93)
+	.switch
+		padding:12px 18px
+		border-bottom:1px solid rgba(7,17,27,.1)
+		&.on
+			.icon-check_circle
+				color:#00c850
+		.icon-check_circle
+			font-size:24px
+			color:rgb(147,153,159)
+			line-height:24px
+			margin-right:4px
+		.text
+			font-size:12px
+			color:rgb(147,153,159)
+			line-height:24px
+			vertical-align:top
+			margin-top:6px
 </style>
